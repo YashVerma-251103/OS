69dd61265b551bf0cea5bf7e8252b0ce1fae7555
Yash Verma <yash23610@iiitd.ac.in>
2024-11-04 10:23:41 +0530

    Starting Assignment 4 -- Simple Smart Loader

---
+++ A4-Assignment_4-SimpleSmartLoader/LastFiles/loader.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <elf.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <signal.h>
+
+#define PAGE_SIZE 4096
+
+struct assigned_memory
+{
+  void *memory;
+  struct assigned_memory *next;
+};
+
+void load_and_run_elf(char* exe);
+void loader_cleanup();

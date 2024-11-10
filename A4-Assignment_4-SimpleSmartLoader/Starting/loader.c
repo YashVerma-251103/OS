f67ca47ab26feaf629acfe415db64431b51a3356
Yash Verma <yash23610@iiitd.ac.in>
2024-11-10 20:31:02 +0530

    finalizing

--- A4-Assignment_4-SimpleSmartLoader/Starting/loader.c
+++ A4-Assignment_4-SimpleSmartLoader/Starting/loader.c
@@ -1,242 +1,249 @@
+// imports
 #include "loader.h"
 
-// assign global variables
-Elf32_Ehdr *ehdr;
-Elf32_Phdr *phdrs;
-int fd;
-void *virtual_mem;
+// global variables for loader
+elf_header_pointer main_elf_hdr;
+program_header_pointer main_program_hdr;
+file_descriptor elf_file;
 uintptr_t entry_point;
-int page_faults = 0;
-int page_allocations = 0;
 
-int fragmentation = 0;
+void_pointer virtual_memory;
+assigned_memory_pointer memory_list = null_value;
 
-struct assigned_memory *memory;
+page page_faults = 0;
+page page_allocations = 0;
+fragmentation total_internal_fragmentation = 0;
 
-// for taking note of addresses assigned
-struct assigned_memory *allocate_memory(struct assigned_memory *memory, void *address)
+// segmentation fault handler
+static signal_handler segmentation_handler(signal_number signal, siginfo_pointer signal_information, void_pointer context)
 {
+    increment_page_faults;
 
-  if (memory == NULL)
-  {
-    memory = malloc(sizeof(struct assigned_memory));
-    memory->memory = address;
-    return memory;
-  }
+    void_pointer fault_adress = (void_pointer)signal_information->si_addr;
+    void_pointer aligned_fault_address = (void_pointer)((uintptr_t)fault_adress & ~(page_size - 1));
 
-  memory->next = allocate_memory(memory->next, address);
-  return memory;
-}
-
-// for freeing memory of the assigned memory
-void free_memory(struct assigned_memory *memory)
-{
-  if (memory == NULL)
-  {
-    return;
-  }
-  free_memory(memory->next);
-  if (munmap(memory->memory, PAGE_SIZE) == -1) perror("munmap");
-  free(memory);
-}
-
-static void handler(int sig, siginfo_t *info, void *unused)
-{
-  page_faults++;
-  void *addr = (void *)info->si_addr;
-
-  // printf("SEG FAULT at address: %p\n", addr);
-
-  Elf32_Phdr *phdr;
-  int i;
-  for (i = 0; i < ehdr->e_phnum; i++)
-  {
-    phdr = &phdrs[i];
-    if (((void *)(phdr->p_vaddr) <= addr) && ((void *)(phdr->p_vaddr) + phdr->p_memsz > addr))
+    program_header_pointer fault_segment;
+    index segment_index;
+    for (segment_index = 0; segment_index < main_elf_hdr->e_phnum; segment_index++)
     {
-      break;
+        fault_segment = &main_program_hdr[segment_index];
+        if (((void_pointer)fault_segment->p_vaddr <= fault_adress) && ((void_pointer)(fault_segment->p_vaddr) + fault_segment->p_memsz > fault_adress))
+        {
+            break;
+        }
     }
-  }
-  if (i == ehdr->e_phnum)
-  {
-    printf("Not found\n");
-    exit(sig);
-  }
+    if ((segment_index == main_elf_hdr->e_phnum))
+    {
+        perror("Segment not found (segmentation_handler)");
+        exit(EXIT_FAILURE);
+    }
 
-  // map the address at which seg fault occured and copy the data from fd
-  virtual_mem = mmap((void*)addr, PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, phdr->p_offset);
-  int page_num = (((size_t)addr - phdr->p_vaddr)/PAGE_SIZE);
+    // mapping the address at which the segmentaion fault occured and copying the data from file to memory
 
-  // calculate fragmentation
-  fragmentation += (phdr->p_memsz - (page_num*PAGE_SIZE)) >= PAGE_SIZE ? 0 : (PAGE_SIZE*(page_num+1)) - (phdr->p_memsz);
-  page_allocations++;
+    if ((fault_segment->p_filesz < fault_segment->p_memsz) && (fault_adress >= (void_pointer)(fault_segment->p_vaddr + fault_segment->p_filesz)))
+    {
+        // .bss section case : uninitialized data accessed
+        virtual_memory = mmap(aligned_fault_address, page_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    }
+    else
+    {
+        bytes segment_offset = fault_segment->p_offset + (aligned_fault_address - (void_pointer)fault_segment->p_vaddr);
+        virtual_memory = mmap(aligned_fault_address, page_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_FIXED, elf_file, segment_offset);
+    }
 
-  // add current address to memory struct
-  memory = allocate_memory(memory, (void*)virtual_mem);
+    if (virtual_memory == MAP_FAILED)
+    {
+        perror("Could not map memory (segmentation_handler)");
+        exit(EXIT_FAILURE);
+    }
 
-  if (virtual_mem == MAP_FAILED)
-  {
-    perror("mmap");
-    exit(sig);
-  }
+    increment_page_allocations;
+    page page_number = (((bytes)aligned_fault_address - fault_segment->p_vaddr) / page_size);
+
+    // add current memory to the list.
+    memory_list = allocate_memory(memory_list, (void_pointer)virtual_memory);
+    
+    // calculating internal fragmentation
+    if ((fault_segment->p_memsz - (page_number * page_size)) < page_size)
+    {
+        total_internal_fragmentation += ((page_size*(page_number + 1)) - fault_segment->p_memsz);
+    }
 }
 
-/*
- * release memory and other cleanups
- */
+// functions for memory management
+void free_memory(assigned_memory_pointer memory)
+{
+    if null_check(memory)
+    {
+        return;
+    }
+
+    free_memory(memory->next);
+    if failure_check(munmap(memory->memory, page_size))
+    {
+        perror("Could not unmap memory (free_memory)");
+    }
+    free(memory);
+}
+assigned_memory_pointer allocate_memory(assigned_memory_pointer memory_list, void_pointer address)
+{
+    if null_check(memory_list)
+    {
+        memory_list = malloc(sizeof(assigned_memory));
+        memory_list->memory = address;
+        return memory_list;
+    }
+
+    memory_list->next = allocate_memory(memory_list->next, address);
+    return memory_list;
+}
+
+// functions for loader
 void loader_cleanup()
 {
-  if (close(fd) == -1)
-  {
-    perror("close");
-    exit(1);
-  }
+    if failure_check(close_file(elf_file))
+    {
+        perror("Could not close file (loader_cleanup)");
+        exit(EXIT_FAILURE);
+    }
 
-  free(ehdr);
-  free(phdrs);
+    free(main_elf_hdr);
+    free(main_program_hdr);
 
-  free_memory(memory);
+    free_memory(memory_list);
 }
-
-/*
- * Load and run the ELF executable file
- */
-void load_and_run_elf(char *exe)
+void load_and_run_elf(string file_path)
 {
-  if (exe == NULL)
-  {
-    printf("Not a valid ELF executabl\n");
-    exit(1);
-  }
+    if null_check(file_path)
+    {
+        perror("File path is null (load_and_run_elf)");
+        exit(EXIT_FAILURE);
+    }
 
-  fd = open(exe, O_RDONLY);
-  // 1. Load entire binary content into the memory from the ELF file.
+    elf_file = open_file(file_path);
+    if failure_check(elf_file)
+    {
+        perror("Could not open file (load_and_run_elf)");
+        exit(EXIT_FAILURE);
+    }
 
-  if (fd == -1)
-  {
-    printf("Error in opening file\n");
-    return;
-  }
+    // getting the elf header
+    main_elf_hdr = malloc(sizeof(elf_header));
+    if failure_check(read_file(elf_file, main_elf_hdr, sizeof(elf_header)))
+    {
+        perror("Could not read file (load_and_run_elf : elf header)");
+        exit(EXIT_FAILURE);
+    }
 
-  ehdr = malloc(sizeof(Elf32_Ehdr));
+    // getting the program header
+    main_program_hdr = malloc((sizeof(program_header) * main_elf_hdr->e_phnum));
+    if failure_check(file_seek(elf_file, main_elf_hdr->e_phoff, SEEK_SET))
+    {
+        perror("Could not seek file (load_and_run_elf : program header)");
+        exit(EXIT_FAILURE);
+    }
+    if failure_check(read_file(elf_file, main_program_hdr, (sizeof(program_header) * main_elf_hdr->e_phnum)))
+    {
+        perror("Could not read file (load_and_run_elf : program header)");
+        exit(EXIT_FAILURE);
+    }
 
-  // get ELF Header
-  if (read(fd, ehdr, sizeof(Elf32_Ehdr)) == -1)
-  {
-    perror("read");
-    exit(1);
-  }
+    // getting the entry point
+    entry_point = main_elf_hdr->e_entry;
 
-  phdrs = malloc(ehdr->e_phnum * sizeof(Elf32_Phdr));
+    int (*_start)() = (int (*)())((void_pointer)entry_point);
 
-  // get program headers
-  if (lseek(fd, ehdr->e_phoff, SEEK_SET) == -1) {
-    perror("lseek");
-    exit(1);
-  }
-
-  if (read(fd, phdrs, ehdr->e_phnum * sizeof(Elf32_Phdr)) == -1) {
-    perror("read");
-    exit(1);
-  }
-
-  entry_point = ehdr->e_entry;
-
-  // typecast the address and run
-  int (*_start)() = (int (*)())((void *)entry_point);
-
-  // 6. Call the "_start" method and print the value returned from the "_start"
-  int result = _start();
-  printf("User _start return value = %d\n", result);
+    int return_value = _start();
+    printf("User _start return value = %d\n", return_value);
 }
-
-int main(int argc, char **argv)
+// boolean check_elf_file(file_descriptor file, elf_header_pointer elf_hdr)
+boolean check_elf_file(file_descriptor file, elf_header elf_hdr)
 {
-  if (argc != 2)
-  {
-    printf("Usage: %s <ELF Executable> \n", argv[0]);
-    exit(1);
-  }
-  struct sigaction sa;
+    if failure_check(file)
+    {
+        perror("File is null (check_elf_file)\n");
+        return false;
+    }
 
-  // seg fault signal handerl
-  sa.sa_flags = SA_SIGINFO | SA_NODEFER;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_sigaction = handler;
+    if (!magic_number_check(elf_hdr))
+    {
+        printf("Magic number check failed (check_elf_file)\n");
+        return false;
+    }
 
-  if (sigaction(SIGSEGV, &sa, NULL) == -1)
-    perror("sigaction");
+    if (!executable_check(elf_hdr))
+    {
+        printf("File is not an executable (check_elf_file)\n");
+        return false;
+    }
 
-  // 1. carry out necessary checks on the input ELF file
-  const char *elf_p = argv[1];
-  int elf_f = open(elf_p, O_RDONLY);
-  Elf32_Ehdr elf_h;
-  read(elf_f, &elf_h, sizeof(Elf32_Ehdr));
-  
-  // 1. carry out necessary checks on the input ELF file
-  // Checking if file is NULL or not
-  if (elf_f == -1)
-  {
-    printf("No elf file found\n");
-    exit(1);
-  }
+    if (!section_header_number_check(elf_hdr))
+    {
+        printf("Invalid section header number (check_elf_file)\n");
+        return false;
+    }
 
-  // Checking Magic Number
-  if (memcmp(elf_h.e_ident, ELFMAG, SELFMAG) != 0)
-  {
-    printf("Invalid ELF File\n");
-    close(elf_f);
-    exit(1);
-  }
+    if (!section_header_size_check(elf_hdr))
+    {
+        printf("Invalid section header size (check_elf_file)\n");
+        return false;
+    }
 
-  // Checking the whether it is executable or not
-  if (elf_h.e_type != ET_EXEC)
-  {
-    printf("Not an executable ELF file\n");
-    close(elf_f);
-    exit(1);
-  }
+    if (!program_header_offset_check(elf_hdr))
+    {
+        printf("Invalid program header offset (check_elf_file)\n");
+        return false;
+    }
 
-  if (elf_h.e_shnum == 0 || elf_h.e_shnum > SHN_LORESERVE)
-  {
-    printf("Invalid number of section headers\n");
-    exit(1);
-  }
+    if (!program_header_number_check(elf_hdr))
+    {
+        printf("Invalid program header number (check_elf_file)\n");
+        return false;
+    }
 
-  if (elf_h.e_shentsize != sizeof(Elf32_Shdr))
-  {
-    printf("Invalid section header size\n");
-    exit(1);
-  }
+    if (!program_header_size_check(elf_hdr))
+    {
+        printf("Invalid program header size (check_elf_file)\n");
+        return false;
+    }
 
-  // Checking all properties of Section Headers
-  if (elf_h.e_phoff >= elf_h.e_phnum * elf_h.e_phentsize)
-  {
-    printf("Invalid Offset for Program Header\n");
-    exit(1);
-  }
+    return true;
+}
+int main(take_input)
+{
+    if (argument_count < 2)
+    {
+        printf("Usage: %s <executable>\n", argument_values[0]);
+        exit(EXIT_FAILURE);
+    }
 
-  if (elf_h.e_phnum == 0 || elf_h.e_phnum > PN_XNUM)
-  {
-    printf("Invalid number of program headers\n");
-    exit(1);
-  }
+    // setting up the signal handler
+    signal_action action;
+    action.sa_flags = SA_SIGINFO | SA_NODEFER;
+    sigemptyset(&action.sa_mask);
+    action.sa_sigaction = segmentation_handler;
+    if failure_check(sigaction(SIGSEGV, &action, null_value))
+    {
+        perror("Could not set signal handler (main : sigaction)");
+        exit(EXIT_FAILURE);
+    }
 
-  if (elf_h.e_phentsize != sizeof(Elf32_Phdr))
-  {
-    printf("Invalid program header size\n");
-    exit(1);
-  }
+    const string file_path = executable_file_path;
+    elf_file = open_file(file_path);
+    elf_header elf_hdr;
+    read_file(elf_file, &elf_hdr, sizeof(elf_header));
+    boolean file_check = check_elf_file(elf_file, elf_hdr);
+    if false_check(file_check)
+    {
+        perror("File is not an ELF file (main)\n");
+        exit(EXIT_FAILURE);
+    }
 
-  close(elf_f);
-  // 2. passing it to the loader for carrying out the loading/execution
-  load_and_run_elf(argv[1]);
-  // 3. invoke the cleanup routine inside the loader
-  loader_cleanup();
+    load_and_run_elf(executable_file_path);
 
-  printf("Total page faults: %d\n", page_faults);
-  printf("Total page allocations: %d\n", page_allocations);
-  printf("Total internal fragmentation: %f Kb\n", (float)fragmentation / 1024.0);
+    loader_cleanup();
 
-  return 0;
+    print_results(page_faults, page_allocations, total_internal_fragmentation);
+
+    return EXIT_SUCCESS;
 }
